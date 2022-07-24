9b921d5942089b94915d5a185cc7bc37ff66c863
Sofía González <sofia.gonzalez.10147@gmail.com>
2022-07-24 08:58:27 +0200

    eureka-server done

---
+++ primers-service/src/main/java/com/ironhack/primersservice/PrimersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.primersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimersServiceApplication.class, args);
+	}
+
+}
---
+++ primers-service/src/test/java/com/ironhack/primersservice/PrimersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.primersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
